7144b81d86d7bfb48e6168bbaf4b55dc0cbfbe73
Mónica Ramos Dávila <monicavrd84@gmail.com>
2023-02-06 13:48:00 -0300

    prueba2

--- titanic.py
+++ titanic.py
@@ -23,14 +23,12 @@ def read_titanic():
 read_titanic()
 df_titanic = read_titanic()
 
-<<<<<<< HEAD
+
 language = st.checkbox('Marque esta casilla si quiere ver la página en inglés')
 if language:
-    localizator = gettext.translation('base', localedir='locales', languages=['en'])
-=======
-
-localizator.install('enGB')
-_ = localizator.gettext 
+    gettext.gettext.translation('base', localedir='locales', languages=['en'])
+    gettext.install('enGB')
+    _ = gettext.gettext 
     
 
 agree = st.checkbox(_('¿Quiere ver las primeras entradas del dataset?'))
